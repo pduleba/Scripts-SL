bda9c72af00ff6282c94e4edfaf271be55a84aee
pduleba <pduleba@pgs-soft.com>
2019-04-18 16:32:55 +0200

    Unix :: Clean up broken package installation

---
+++ cleanup-package.sh
@@ -0,0 +1,5 @@
+# Remove package
+sudo mv /var/lib/dpkg/info/PACKAGE_NAME* /tmp/
+sudo dpkg --remove --force-remove-reinstreq PACKAGE_NAME
+sudo apt-get remove PACKAGE_NAME
+sudo apt-get autoremove && sudo apt-get autoclean
