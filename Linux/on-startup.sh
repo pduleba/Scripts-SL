b89e1ef6537e25aca2433dd389d6c4679caf5134
pduleba <pduleba@pgs-soft.com>
2019-05-09 08:35:36 +0200

    Linux :: run on start

---
+++ Linux/on-startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# ------------ ON NEW BASH SESSION START ------------
+# add command -> `/etc/bash.bashrc` -> `any` user
+# add command -> `~/.bashrc` 		-> `current` user 
+# ------------ ON LOGIN  ------------
+# add text	  -> `/etc/motd`		-> `login` or `ssh`
